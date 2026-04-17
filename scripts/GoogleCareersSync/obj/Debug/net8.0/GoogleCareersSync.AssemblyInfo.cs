dc8eae3037c3afa8065f9403618ef74f7ded53db
igor.kravchenko <igor.kravchenko@techtellent.io>
2026-04-17 17:28:28 +0100

    fix

--- scripts/GoogleCareersSync/obj/Debug/net8.0/GoogleCareersSync.AssemblyInfo.cs
+++ scripts/GoogleCareersSync/obj/Debug/net8.0/GoogleCareersSync.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoogleCareersSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190c4bfb64b91a309f43ff2af17a3d23c2d959a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6c6d67ccfe0d21fb5aa4cf7c2372c4bc98741d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoogleCareersSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoogleCareersSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
